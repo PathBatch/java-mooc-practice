e4faa83047101465ef08092a79f297b2f2c0c9b9
PathBatch <brendanvbatchelder@gmail.com>
2025-08-10 21:44:50 -0500

    Fix: replace Sandbox with clean updated version

--- Part01/part01-Part01_01.Sandbox/src/main/java/Sandbox.java
+++ Part01/part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -1,8 +1,14 @@
 
+import java.util.Scanner;
+
 public class Sandbox {
 
     public static void main(String[] args) {
-        // Write your program here
         System.out.println("This is the beginning");
+        //Below is my attempt at adding a scanner
+        Scanner scannertool = new Scanner (System.in);
+        System.out.println("Please write below:");
+        String response = scannertool.nextLine();
+        System.out.println(response);
     }
 }
