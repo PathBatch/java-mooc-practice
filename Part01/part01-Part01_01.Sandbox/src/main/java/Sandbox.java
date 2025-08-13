273138a490d6dfa79abf980844d60dd71434d4d5
PathBatch <brendanvbatchelder@gmail.com>
2025-08-12 19:40:42 -0500

    Updated Part01.01 Sandbox to test booleans and conditionals

--- Part01/part01-Part01_01.Sandbox/src/main/java/Sandbox.java
+++ Part01/part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -5,10 +5,18 @@ public class Sandbox {
 
     public static void main(String[] args) {
         System.out.println("This is the beginning");
-        //Below is my attempt at adding a scanner
+        //Below is my attempt at using boolean for a recreation of larger than or equal to
         Scanner scannertool = new Scanner (System.in);
-        System.out.println("Please write below:");
-        String response = scannertool.nextLine();
-        System.out.println(response);
+        System.out.println("Give a number:");
+        int firstnum = Integer.valueOf(scannertool.nextLine());
+        System.out.println("Give another number:");
+        int secondnum = Integer.valueOf(scannertool.nextLine());
+        boolean isMoreThan = firstnum > secondnum;
+        if (isMoreThan) {
+            System.out.println(firstnum + " is greater than " + secondnum);
+        } else {
+            System.out.println(firstnum + " is not greater than " + secondnum);
+        }
+        
     }
 }
