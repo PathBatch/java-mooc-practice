d21a032a72092226c0052565915b43a9b6467da8
PathBatch <brendanvbatchelder@gmail.com>
2025-08-12 20:20:28 -0500

    Added Part01.37 GiftTax

---
+++ Part01/part01-Part01_37.GiftTax/src/main/java/GiftTax.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+public class GiftTax {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Value of the gift?");
+        int value = Integer.valueOf(scan.nextLine());
+        double tax = 0;
+        if (value >= 1000000) {
+            tax = 142100 + ((value - 1000000) * .17);
+        } else if (value >= 200000) {
+            tax = 22100 + ((value - 200000) * .15);
+        } else if (value >= 55000) {
+            tax = 4700 + ((value - 55000) * .12);
+        } else if (value >= 25000) {
+            tax = 1700 + ((value - 25000) * .10);
+        } else if (value >= 5000) {
+            tax = 100 + ((value - 5000) * .08);
+        }
+        if (tax != 0) {
+            System.out.println("Tax: " + tax);
+        } else {
+            System.out.println("No tax!");
+        }
+        
+
+    }
+}
