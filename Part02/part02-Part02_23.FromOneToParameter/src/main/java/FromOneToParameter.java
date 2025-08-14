f194d4311dedd2d581db69f71278f1c7dc04adba
PathBatch <brendanvbatchelder@gmail.com>
2025-08-13 22:42:06 -0500

    Added Part02.23 FromOneToParameter

---
+++ Part02/part02-Part02_23.FromOneToParameter/src/main/java/FromOneToParameter.java
@@ -0,0 +1,16 @@
+
+
+public class FromOneToParameter {
+
+    public static void main(String[] args) {
+        printUntilNumber(5);
+
+    }
+    
+    public static void printUntilNumber (int end) {
+        for (int i = 1; i <=end; i++) {
+            System.out.println(i);
+        }
+    }
+
+}
