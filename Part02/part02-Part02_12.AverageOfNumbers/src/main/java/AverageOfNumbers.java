399e44f609f9c798a3ccae4b1dfc8fd82dedfaa2
PathBatch <brendanvbatchelder@gmail.com>
2025-08-13 20:59:54 -0500

    Added Part02.12 AverageOfNumbers

---
+++ Part02/part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class AverageOfNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numberCount = 0;
+        int numberSum = 0;
+        while (true) {
+            System.out.println("Give a number:");
+            int input = Integer.valueOf(scanner.nextLine());
+            if (input == 0) {
+                break;
+            }
+            numberCount = numberCount + 1; 
+            numberSum = numberSum + input; 
+        }
+        System.out.println("Average of the numbers: " + ( (double) numberSum / numberCount));
+
+    }
+}
