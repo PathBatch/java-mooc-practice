f5646ee389c61cd7406221e55375a13582e451a2
PathBatch <brendanvbatchelder@gmail.com>
2025-08-22 22:25:51 -0500

    Updated Part03.31 Removed Unnecessary loop and optimized code

--- Part03/part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
+++ Part03/part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
@@ -6,22 +6,21 @@ public class NameOfTheOldest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int oldest = 0;
-        String name = "Nothing";
+        String oldestName = "";
         while (true) {
             String line = scanner.nextLine();
             if (line.equals("")) {
                 break;
             }
             String[] parts = line.split(",");
-            for (int i = 1; i < parts.length; i = i + 2) {
-                int age = Integer.valueOf(parts[i]);
-                if (age > oldest) {
-                    oldest = age;
-                    name = parts[i -1];
-                }
+            String name = parts[0];
+            int age = Integer.valueOf(parts[1]);
+            if (age > oldest) {
+                oldest = age;
+                oldestName = name;
             }
         }
-        System.out.println("Name of the oldest: " + name);
+        System.out.println("Name of the oldest: " + oldestName);
 
 
     }
