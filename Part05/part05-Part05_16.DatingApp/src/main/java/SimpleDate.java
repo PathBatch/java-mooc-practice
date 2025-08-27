dbf6ed7b04b5bfa74807970d2c95fd15a7faf7f3
PathBatch <brendanvbatchelder@gmail.com>
2025-08-26 23:49:34 -0500

    Added Part05.16 DatingApp

---
+++ Part05/part05-Part05_16.DatingApp/src/main/java/SimpleDate.java
@@ -0,0 +1,60 @@
+
+public class SimpleDate {
+
+    private int day;
+    private int month;
+    private int year;
+
+    public SimpleDate(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+    
+    public void advance(int howManyDays) {
+        for (int i = 0; i < howManyDays; i++) {
+            this.advance();
+        }
+    }
+    
+    public void advance() {
+        day += 1;
+        if (day > 30) {
+            month += 1;
+            day = 1;
+        }
+        if (month > 12) {
+            month = 1;
+            year += 1;
+        }
+    }
+    
+    public SimpleDate afterNumberOfDays(int days) {
+        SimpleDate newDate = new SimpleDate(this.day, this.month, this.year);
+        newDate.advance(days);
+        return newDate;
+    }
+
+    @Override
+    public String toString() {
+        return this.day + "." + this.month + "." + this.year;
+    }
+
+    public boolean before(SimpleDate compared) {
+        if (this.year < compared.year) {
+            return true;
+        }
+
+        if (this.year == compared.year && this.month < compared.month) {
+            return true;
+        }
+
+        if (this.year == compared.year && this.month == compared.month &&
+                 this.day < compared.day) {
+            return true;
+        }
+
+        return false;
+    }
+
+}
