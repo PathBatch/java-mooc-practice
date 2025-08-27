95ed4daf6195e12a9d31e8c5c85fb68e2632afbf
PathBatch <brendanvbatchelder@gmail.com>
2025-08-26 22:05:27 -0500

    Added Part05.14 Books

---
+++ Part05/part05-Part05_14.Books/src/main/java/Book.java
@@ -0,0 +1,36 @@
+
+public class Book {
+
+    private String name;
+    private int publicationYear;
+
+    public Book(String name, int publicationYear) {
+        this.name = name;
+        this.publicationYear = publicationYear;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Book object = (Book) obj;
+        if (this.name.equals(object.name) 
+                && this.publicationYear == object.publicationYear) {
+            return true;
+        }
+        return false;
+    }
+
+}
