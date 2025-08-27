8310d479ed6d99b13d09d55fc1fc692adaeb264d
PathBatch <brendanvbatchelder@gmail.com>
2025-08-26 21:12:44 -0500

    Added Part05.13 IdenticalTwins

---
+++ Part05/part05-Part05_13.IdenticalTwins/src/main/java/Person.java
@@ -0,0 +1,39 @@
+
+import java.util.Objects;
+
+public class Person {
+
+    private String name;
+    private SimpleDate birthday;
+    private int height;
+    private int weight;
+
+    public Person(String name, SimpleDate birthday, int height, int weight) {
+        this.name = name;
+        this.birthday = birthday;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    // implement an equals method here for checking the equality of objects
+    @Override
+    public boolean equals(Object compared) {
+        if (this == compared) {
+            return true;
+        }
+        if (compared == null) {
+            return false;
+        }
+        if (getClass() !=compared.getClass()) {
+            return false;
+        }
+        Person personTwo = (Person) compared;
+        if (this.name.equals(personTwo.name)
+                && this.height == personTwo.height
+                && this.weight == personTwo.weight
+                && this.birthday.equals(personTwo.birthday)) {
+            return true;
+        }
+        return false;
+    }
+}
