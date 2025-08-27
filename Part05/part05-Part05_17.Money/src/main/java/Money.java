9025638cd1dc5ba087b8a1defdd665906517e491
PathBatch <brendanvbatchelder@gmail.com>
2025-08-26 23:49:53 -0500

    Added Part05.17 Money

---
+++ Part05/part05-Part05_17.Money/src/main/java/Money.java
@@ -0,0 +1,71 @@
+
+public class Money {
+
+    private final int euros;
+    private final int cents;
+
+    public Money(int euros, int cents) {
+
+        if (cents > 99) {
+            euros = euros + cents / 100;
+            cents = cents % 100;
+        }
+
+        this.euros = euros;
+        this.cents = cents;
+    }
+
+    public int euros() {
+        return this.euros;
+    }
+
+    public int cents() {
+        return this.cents;
+    }
+
+    public String toString() {
+        String zero = "";
+        if (this.cents < 10) {
+            zero = "0";
+        }
+
+        return this.euros + "." + zero + this.cents + "e";
+    }
+    
+    public Money plus(Money addition) {
+        int helper = this.euros + addition.euros;
+        int helper2 = this.cents + addition.cents;
+        if (helper2 >= 100) {
+            helper += helper2 / 100;
+            helper2 = helper2 % 100;
+        }
+        Money newMoney = new Money(helper, helper2);
+        return newMoney;
+    }
+    
+    public Money minus(Money decreaser) {
+        int helper = this.euros - decreaser.euros;
+        int helper2 = this.cents - decreaser.cents;
+        if (helper2 < 0) {
+            helper -= 1;
+            helper2 += 100;
+        }
+        if (helper < 0) {
+            helper = 0;
+            helper2 = 0;
+        }
+        Money newMoney = new Money(helper, helper2);
+        return newMoney;
+    }
+    
+    public boolean lessThan(Money compared) {
+        if (this.euros < compared.euros) {
+            return true;
+        }
+        if (this.euros == compared.euros && this.cents < compared.cents) {
+            return true;
+        }
+        return false;
+    }
+
+}
